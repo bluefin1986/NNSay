af79d09d44e26f3dee989a0f388bd13412863e68
bluefin <bluefin8603@gmail.com>
2024-01-22 17:12:12 +0800

    Initial Commit

---
+++ NiuNiuSay/NiuNiuSayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiuNiuSayApp.swift
+//  NiuNiuSay
+//
+//  Created by 郑敏嘉 on 2024/1/22.
+//
+
+import SwiftUI
+
+@main
+struct NiuNiuSayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
